6da835d960c322fe42f2b5dad0860ba6fc6dc8fa
ojondhale-r7 <onkar_jondhale@rapid7.com>
2026-06-01 17:50:15 +0530

    Update formula

--- Formula/hello-world.rb
+++ Formula/hello-world.rb
@@ -5,8 +5,16 @@ class HelloWorld < Formula
   sha256 "6fcbacba15d416fb63bef145db52505795d7f86cf43a5cf9bf601f30d06bbcc8"
 
   def install
-    # Explicitly look inside the folder GitHub creates upon extraction
-    bin.install "homebrew-hello-world-1.0.0/hello-world"
+    # Look for the 'hello-world' script anywhere in the extracted directory
+    script_path = Dir["**/hello-world"].first
+    
+    if script_path.nil?
+      # If it still can't find it, fail gracefully and print the contents to help debug
+      odie "Could not find 'hello-world' script. Contents of build directory: #{Dir.entries('.')}"
+    end
+
+    # Install the script into Homebrew's binary directory and rename it to 'hello-world'
+    bin.install script_path => "hello-world"
   end
 
   test do

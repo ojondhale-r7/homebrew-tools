8e15427ed1d26e0f03be63d2fcf572231dadb228
ojondhale-r7 <onkar_jondhale@rapid7.com>
2026-06-01 17:43:31 +0530

    Update sha

--- Formula/hello-world.rb
+++ Formula/hello-world.rb
@@ -2,7 +2,7 @@ class HelloWorld < Formula
   desc "A simple hello world shell script CLI"
   homepage "https://github.com/ojondhale-r7/homebrew-hello-world"
   url "https://github.com/ojondhale-r7/homebrew-hello-world/archive/refs/tags/1.0.0.tar.gz"
-  sha256 "6fcbacba15d416fb63bef145db52505795d7f86cf43a5cf9bf601f30d06bbcc8  -"
+  sha256 "6fcbacba15d416fb63bef145db52505795d7f86cf43a5cf9bf601f30d06bbcc8"
 
   def install
     # This takes your 'hello-world' shell script from the archive 
